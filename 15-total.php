ae8e1f0118d7961e886cc13b52a9932354affdec
Rachel <rac777blue@hotmail.com>
2020-02-10 20:17:22 +0000

    completed challenge 15

---
+++ 15-total.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+function total(array $items) : float {
+    
+    $total = 0;
+
+    foreach ($items as $item) {
+
+        $total += $item["price"];
+    }
+
+    return $total;
+};
+
+$shoppingList = [[
+    "name" => "coffee",
+    "price" => 3.50,
+], [
+    "name" => "tea",
+    "price" => 2.50,
+], [
+    "name" => "toblerone",
+    "price" => 3.20,
+], [
+    "name" => "newspaper",
+    "price" => 0.10,
+]];
+
+var_dump(
+    total($shoppingList), // float(9.3)
+);
+
+// Create a function, total, that takes an array of shopping item associative arrays and adds their price together.
