c2b309921c6d07f0e563d4f1bca21394ee2ec70d
Rachel <rac777blue@hotmail.com>
2020-02-10 15:49:22 +0000

    tidied up first challenge

--- 01-concat.php
+++ 01-concat.php
@@ -9,5 +9,4 @@ function concat (string $firstWord, string $secondWord) : string {
 var_dump(concat("fish", "sticks")); // string(11) "fish sticks"
 var_dump(concat("hello", "world")); // string(11) "hello world"
 
-
 // Create a function, concat, that takes two strings as arguments. The function should return the two strings joined together with a space in between.
