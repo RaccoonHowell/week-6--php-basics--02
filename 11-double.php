52c3eaebffc9bb2f375ffbaf3bf491821e020410
Rachel <rac777blue@hotmail.com>
2020-02-10 17:23:43 +0000

    completed challenge 11

---
+++ 11-double.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+function double (array $numbers) : array {
+
+    $newArray = [];
+
+    foreach ($numbers as $value) {
+
+        $newArray[] = $value * 2;
+    }
+
+    return $newArray;
+}
+
+var_dump(
+    double([2, 3, 4, 5, 6]), // [4, 6, 8, 10, 12]
+    double([1, 2, 5]), // [2, 4, 10]
+);
+
+// Create a function, double, that takes an array of numbers as an argument. Return a new array containing each number doubled.
